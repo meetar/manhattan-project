e093ff3763e98266380fbca6521552327272b606
Peter Richardson <github@zoomy.net>
2015-04-16 15:32:15 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,63 @@
+/*jslint browser: true*/
+/*global Tangram, gui */
+
+(function () {
+    'use strict';
+
+    var locations = {
+        'London': [51.508, -0.105, 15],
+        'New York': [40.70531887544228, -74.00976419448853, 15],
+        'Seattle': [47.609722, -122.333056, 15]
+    };
+
+    var map_start_location = locations['New York'];
+
+    /*** URL parsing ***/
+
+    // leaflet-style URL hash pattern:
+    // #[zoom],[lat],[lng]
+    var url_hash = window.location.hash.slice(1, window.location.hash.length).split('/');
+
+    if (url_hash.length == 3) {
+        map_start_location = [url_hash[1],url_hash[2], url_hash[0]];
+        // convert from strings
+        map_start_location = map_start_location.map(Number);
+    }
+
+    /*** Map ***/
+
+    var map = L.map('map',
+        {'keyboardZoomOffset': .05}
+    );
+
+
+    var layer = Tangram.leafletLayer({
+        scene: 'styles.yaml',
+        attribution: 'Map data &copy; OpenStreetMap contributors | <a href="https://github.com/tangrams/tangram" target="_blank">Source Code</a>'
+    });
+
+    window.layer = layer;
+    var scene = layer.scene;
+    window.scene = scene;
+
+    map.setView(map_start_location.slice(0, 2), map_start_location[2]);
+
+    var hash = new L.Hash(map);
+    
+    // Resize map to window
+    function resizeMap() {
+        document.getElementById('map').style.width = window.innerWidth + 'px';
+        document.getElementById('map').style.height = window.innerHeight + 'px';
+        map.invalidateSize(false);
+    }
+
+    window.addEventListener('resize', resizeMap);
+    resizeMap();
+
+    window.addEventListener('load', function () {
+        // Scene initialized
+        layer.addTo(map);
+    });
+
+
+}());
